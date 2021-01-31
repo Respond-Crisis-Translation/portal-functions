b36aa155dfe0584f4488d0a36b27e8504e70b812
Yogen Rai <yogen.rai.992@gmail.com>
2021-01-31 17:04:40 -0600

    Update function to create user and send password reset email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+import { UserRecord } from "firebase-functions/lib/providers/auth";
+
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 
@@ -7,8 +9,8 @@ admin.initializeApp(functions.config().firebase);
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "username",
-    pass: "password",
+    user: "",
+    pass: "",
   },
 });
 
@@ -20,25 +22,69 @@ export const sendInvitationEmail = functions.firestore
     const translatorEmail = after.email;
     console.log("translator email: ", translatorEmail);
     if (before.status !== after.status && after.status === "APPROVED") {
-      const emailSubject =
-        "Welcome to the Respond: Crisis Translators Network!";
-      const emailBody = `Hello ${after.first_name}, Please proceed to create your login inforamtion. Thank you`;
-      const mailOptions = {
-        from:
-          "Respond: Crisis Translators Network <respond@crisistranslators.net>",
-        to: translatorEmail,
-        subject: emailSubject,
-        text: emailBody,
-      };
+      // if user hasn't been created,
+      // then create user, generate password reset link and send email
+      console.log("translator uid: ", context.params.id);
 
-      transporter.sendMail(mailOptions, function (error: any, info: any) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Email sent: " + info.response);
-        }
-      });
+      admin
+        .auth()
+        .createUser({
+          uid: context.params.id,
+          email: translatorEmail,
+          password: randomString(10),
+          emailVerified: true,
+          displayName: after.first_name,
+          disabled: false,
+        })
+        .then((userRecord: UserRecord) => {
+          // See the UserRecord reference doc for the contents of userRecord.
+          console.log("Successfully created new user:", userRecord.uid);
+
+          admin
+            .auth()
+            .generatePasswordResetLink(userRecord.email, {
+              url: "https://rct-portal.web.app/",
+            })
+            .then((link: string) => {
+              // Construct password reset email template, embed the link and send
+              // using custom SMTP server.
+              console.log("Successfully created link:", link);
+              return sendEmail(userRecord, link);
+            })
+            .catch((error: any) => {
+              // Some error occurred.
+              console.log("Error send an reset email: ", error);
+            });
+        })
+        .catch((error: any) => {
+          console.log("Error creating new user: ", error);
+        });
     }
-
     return new Promise((res: any, err: any) => true);
   });
+
+function sendEmail(user: UserRecord, link: string) {
+  const emailSubject = "Welcome to the Respond: Crisis Translators Network!";
+  const emailBody = `Hello ${user.displayName}, Please proceed to create your login information by clicking ${link}. Thank you`;
+  const mailOptions = {
+    from: "Respond: Crisis Translators Network <respond@crisistranslators.net>",
+    to: user.email,
+    subject: emailSubject,
+    text: emailBody,
+  };
+
+  transporter.sendMail(mailOptions, function (error: any, info: any) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
+
+function randomString(len: number) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(len);
+  });
+}
